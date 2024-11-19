377d46375541c37ed4e2566aa01d307b1386bb96
SUNISH <sunish@SUNISHs-MacBook-Air.local>
2024-11-19 09:11:17 +0530

    remove bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const SecretKey = require("../models/SecretKey");
-const bcrypt = require('bcrypt');
 
 // @route   POST /api/auth/verify
 // @desc    Verify the secret key for authentication
